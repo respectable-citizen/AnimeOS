bd351d6e6295c3d2d945973d4661cf3d0eb5ea9b
catgirladdict <hillsdms@protonmail.ch>
2021-05-08 19:30:43 +0800

    Add primitive text scaling implementation

--- kernel/text_renderer/text_renderer.cpp
+++ kernel/text_renderer/text_renderer.cpp
@@ -9,9 +9,11 @@ namespace TextRenderer {
 		uint16_t m_cursor_x;
 		uint16_t m_cursor_y;
 		uint32_t m_color;
+		uint32_t scale_factor;
 	}
 
 	void initialise(GraphicsInfo graphics_info) {
+		scale_factor = 3; //Each pixel of the 8x8 font is actually 3x3 pixels, this might be changd or even decided dynamically in the future
 		m_graphics_info = graphics_info;
 		set_cursor_x(0);
 		set_cursor_y(0);
@@ -51,8 +53,15 @@ namespace TextRenderer {
 		return m_color;
 	}
 	
-	void draw_pixel(uint32_t x, uint32_t y) {
-		m_graphics_info.address[y * m_graphics_info.pixels_per_scanline + x] = color();
+	inline void draw_pixel(uint32_t x, uint32_t y) {
+		uint32_t scaled_x = x * scale_factor;
+		uint32_t scaled_y = y * scale_factor;
+
+		for (uint32_t x_offset = 0; x_offset < scale_factor; x_offset++) {
+			for (uint32_t y_offset = 0; y_offset < scale_factor; y_offset++) {
+				m_graphics_info.address[(scaled_y + y_offset) * m_graphics_info.pixels_per_scanline + (scaled_x + x_offset)] = color();
+			}
+		}
 	}
 	
 	void draw_character(uint8_t c) {
