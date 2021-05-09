9b73d9273a90129b8867399340411c1dd1b1a43d
catgirladdict <hillsdms@protonmail.ch>
2021-05-09 16:31:04 +0800

    Start work on rewriting heap

--- kernel/memory_manager/heap.cpp
+++ kernel/memory_manager/heap.cpp
@@ -1,14 +1,29 @@
 #include <stdint.h>
 
 #include "pmm.hpp"
-#include "block32.hpp"
+#include "../lib/vector.hpp"
 #include "../text_renderer/text_renderer.hpp"
 
 namespace Heap {
 	namespace {
+		struct HeapDescriptor {
+			void *start;
+			uint64_t size;
+			bool free;
+		};
+
+		void *m_heap;
+		Vector<HeapDescriptor> m_heap_descriptors;
 	}
 
 	void initialise() {
+		uint64_t page_count = 64;
+		m_heap = PMM::allocate_kernel_pages(page_count);
+		HeapDescriptor initial_descriptor;
+		initial_descriptor.start = m_heap;
+		initial_descriptor.size = PMM::pages_to_bytes(page_count);
+		initial_descriptor.free = 1;
+		m_heap_descriptors.push(initial_descriptor);
 	}
 
 	void* malloc(uint64_t bytes) {
